d65f770fc3f9b84b59fc6f115a6ea5fd192c06a9
stevenkooi <steven.kooi1998@gmail.com>
2023-01-11 11:37:56 +0100

    opzet representation

---
+++ representation.py
@@ -0,0 +1,23 @@
+# Maak voor ieder component een Python class
+class Grid:
+  pass
+
+class House:
+  pass
+
+class Battery:
+  pass
+
+class Cable:
+  pass
+
+# Schrijf vervolgens een functie die een instantie van het probleem 
+# inleest en op basis daarvan instanties (objecten) van de verschillende classes aanmaakt.
+def read_grid(var):
+  pass
+
+
+# Schrijf dan nog één functie, die de objecten van de verschillende classes inneemt en 
+# output in het voorgeschreven format genereerd.
+def get_output(var):
+  pass
