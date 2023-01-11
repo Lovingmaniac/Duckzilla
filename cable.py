709618925e4857c45f25b1788d4b857bdc5e558d
Lovingmaniac <evaviebie@gmail.com>
2023-01-11 13:45:28 +0100

    start house en cable class

--- cable.py
+++ cable.py
@@ -1,2 +1,5 @@
 class Cable:
-  pass
+   
+    pass        
+
+  
--- house.py
+++ house.py
@@ -1,2 +1,20 @@
 class House:
-  pass
+    def __innit__(self, x: int, y: int, output: float) -> None:
+        self.location = {x, y}
+        self.output = output
+        self.cables = [tuple]
+
+    def add_cable(self, location: tuple) -> None:
+        '''adds a cable to the house object'''
+        self.cables.append(location)
+
+    def remove_cable(self, location: tuple) -> None:
+        '''removes cable from house object if it is there'''
+        if location not in self.cables:
+            return 1
+        
+        self.cables.remove(location)
+
+    def get_output(self) -> float:
+        '''returns the output of a house object'''
+        return self.output
