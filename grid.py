9f6a1aae2b5412f439a7b74c5528019da549694c
anouprins <aprins@live.com>
2023-01-11 18:51:26 +0100

    add comments, use csv module in grid.py

--- grid.py
+++ grid.py
@@ -1,3 +1,5 @@
+import csv
+
 from battery import Battery
 from house import House
 
@@ -10,28 +12,42 @@ class Grid:
   
   def load_grid(self, district):
     self.district = district
+
     # load battery data
-    with open(f"data/district_{district}/district-{district}_batteries.csv") as f:
-        header = f.readline()
-        for line in f:
-            split = line.strip().split(',')
-            x = int(split[0].strip('"'))
-            y = int(split[1].strip('"'))
-            capacity = float(split[2])
+    with open(f"data/district_{district}/district-{district}_batteries.csv") as csvfile:
+      reader = csv.reader(csvfile)
 
-            self.batteries.append(Battery(x, y, capacity))
+      # skip the header
+      next(reader, None)
 
-    with open(f"data/district_{district}/district-{district}_houses.csv") as f:
-        header = f.readline()
-        for line in f:
-            split = line.strip().split(',')
-            x = int(split[0])
-            y = int(split[1])
-            output = float(split[2])
+      # iterate over each row in csv file
+      for row in reader:
+        position = row[0]
+        capacity = float(row[1])
 
-            self.houses.append(House(x, y, output))
+        # positions are defined in the form "x,y"
+        split = position.split(',')
+        x = int(split[0])
+        y = int(split[1])
 
-        
+        self.batteries.append(Battery(x, y, capacity))
+
+    # load houses data
+    with open(f"data/district_{district}/district-{district}_houses.csv") as csvfile:
+      reader = csv.reader(csvfile)
+
+      # skip the header
+      next(reader, None)
+
+      # iterate over each row in csv file
+      for row in reader:
+        x = int(row[0])
+        y = int(row[1])
+        maxoutput = float(row[2])
+
+        # make a class House object and add it to house list
+        self.houses.append(House(x, y, maxoutput))
+
 if __name__ == '__main__':
     grid = Grid()
     grid.load_grid(1)
