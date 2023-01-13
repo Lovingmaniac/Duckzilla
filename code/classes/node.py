06bb15ff0611b3389a28d7a9a6bfca8e558c8242
Lovingmaniac <evaviebie@gmail.com>
2023-01-13 11:33:29 +0100

    node class uitgewerkt

--- code/classes/node.py
+++ code/classes/node.py
@@ -1,2 +1,10 @@
-class Node:
-    pass
+class Node():
+    def __init__(self, x, y, uid):
+        self.coordinates = (x, y)
+        self.connections = {}
+        self.id = uid
+
+    def add_connection(self, node):
+            self.connections[node.id] = node
+    
+    
