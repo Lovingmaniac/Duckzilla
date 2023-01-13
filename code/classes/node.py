0f3f206b91f420e074cae6c6d0e99276a8455648
Lovingmaniac <evaviebie@gmail.com>
2023-01-13 11:58:51 +0100

    Node class aangepast

--- code/classes/node.py
+++ code/classes/node.py
@@ -1,10 +1,16 @@
 class Node():
+
     def __init__(self, x, y, uid):
         self.coordinates = (x, y)
         self.connections = {}
         self.id = uid
+        self.type = None
 
     def add_connection(self, node):
             self.connections[node.id] = node
     
+    def add_type(self, type):
+        self.type = type
+
     
+
