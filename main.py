86253c9222a97fc365cf54bca851801fd38f83ab
anouprins <aprins@live.com>
2023-01-13 12:42:43 +0100

    add node dictionary in grid.py and add_type for all batteries and houses in loading function

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from code.classes.grid import Grid
 
 if __name__ == "__main__":
     grid = Grid()
+    grid.make_nodes()
     grid.load_grid(1)
-    grid.make_grid()
+    
 
     for item in grid.houses:
         print(item)
