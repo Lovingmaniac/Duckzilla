4cb826c0b2de9c90568f4abc1706373c5b774f89
anouprins <aprins@live.com>
2023-01-30 21:37:41 +0100

    add function that puts values in csv file

--- code/algorithms/iteration.py
+++ code/algorithms/iteration.py
@@ -107,7 +107,7 @@ class Iteration():
         for i in range(5):
             self.mutate_battery_connection(new_model)
 
-    def check_solution(self, new_model: Model) -> None:
+    def check_solution(self, new_model: Model, iteration: int, time_str) -> None:
         """Checks if solution is a better solution than the best solution reached.
         If solution has lower costs and is valid,
         current best costs and model solution are switched to solution model.
@@ -123,11 +123,24 @@ class Iteration():
             return None
 
         if new_solution < old_solution:
-            print("found better solution")
             self.best_costs = new_solution
-            print(new_solution)
             self.model = new_model
             visualize(self.model)
+            self.experiment_file(iteration, self.best_costs, time_str)
+            print("found better solution")
+            print(new_solution)
+
+    def experiment_file(self, iteration: int, total_costs: int, time):
+        with open("output/experiment_iteration.csv","a",newline="") as experiment:
+
+            # creating writer object
+            csv_writer = csv.writer(experiment)
+
+            # make list of values for line
+            line = [f"iteration: {iteration}", f" total costs: {total_costs}", f" time: {time}"]
+            
+            # appending data
+            csv_writer.writerow(line)
 
     def run(self, iterations: int) -> None:
         """Runs the hillclimber algorithm for a specific amount of iterations.
