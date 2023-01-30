bc983d68ced67e91b6707525b5dd34ccbaeaa938
anouprins <aprins@live.com>
2023-01-30 11:46:42 +0100

    random iteration algorithm works

--- code/algorithms/iteration.py
+++ code/algorithms/iteration.py
@@ -5,96 +5,116 @@ from code.visualization.visualization import visualize
 
 """
 to do:
-
-- fix bug in randomise.run(), empty houselist after performance
-- make assertion that only solutions are given
 - gradient descent
-
-- swap houses
 - choose houses per output
--
 
 - parser:
 -- visualise
 -- lowest costs
 -- counter(rounds)
 -- time
---
---
 """
+
 
 class Iteration():
     """Class iteration algorithm
     Class takes a valid solution, and tries to improve it mutating changes for X iterations."""
 
     def __init__(self, model: Model):
-        # if not model.is_solution():
-        #     raise Exception("Please provide a valid solution.")
+        """Constructor of class Iteration:
+        Arguments:
+        model -- a model of class Model with valid solution
+
+        Returns:
+        self.model -- model object that is a copy of given model
+        self.best_costs -- best costs reached in algorithm, starting with solution of given model"""
+        if not model.is_solution():
+            raise Exception("Please provide a valid solution.")
 
         self.model = model.copy()
         self.original_costs = model.calculate_costs()
 
-    def mutate_battery_connection(self, new_model: Model) -> None:
-        """Changes the connection from a random battery to a random house."""
-        # generate a random battery and random house connected to battery
-        idx_first_battery = self.get_rand_battery_idx(new_model)
-        first_battery = self.get_rand_battery(new_model, idx_first_battery)
-        first_house = self.get_rand_house(new_model, idx_first_battery)
+    def mutate_battery_connection(self, new_model: Model):
+        """Switches two random existing connections of model. 
+        Random batteries are generated, with random houses connected to each battery.
+        Houses are swapped, from house lists in batteries. 
+        Old cables are removed and new cables are made.
 
-        # generate another random battery/house connection
-        idx_mutate_battery = self.get_rand_battery_idx(new_model)
-        mutate_battery = self.get_rand_battery(
-            new_model, idx_mutate_battery)
+        Arguments:
+        new_model -- the model of input with existing connections"""
+        # get random batteries
+        rand_battery_1 = self.get_rand_battery(new_model)
+        rand_battery_2 = self.get_rand_battery(new_model)
 
-        # only accept a different battery
-        while idx_first_battery == idx_mutate_battery:
-            idx_mutate_battery = self.get_rand_battery_idx(new_model)
+        # second random battery has to differ from first one
+        while rand_battery_2 == rand_battery_1:
+            rand_battery_2 = self.get_rand_battery(new_model)
 
-        # generate random house connected to random mutate battery
-        mutate_house = self.get_rand_house(new_model, idx_mutate_battery)
+        # generate random houses
+        rand_house_1 = self.get_rand_house(new_model, rand_battery_1)
+        rand_house_2 = self.get_rand_house(new_model, rand_battery_2)
 
-        # remove original cables
-        new_model.remove_one_cable(first_battery, first_house)
-        new_model.remove_one_cable(mutate_battery, mutate_house)
+        # swap houses from battery lists
+        self.swap_houses(rand_battery_1,
+                         rand_battery_2,
+                         rand_house_1,
+                         rand_house_2)
 
-        # add new connections and update capacity of batteries
-        new_model.batteries[idx_first_battery].add_house(mutate_house)
-        new_model.batteries[idx_mutate_battery].add_house(first_house)
-
-        # remove original connections and update capacity of batteries
-        new_model.batteries[idx_first_battery].remove_house(first_house)
-        new_model.batteries[idx_mutate_battery].remove_house(mutate_house)
+        # remove old cables
+        new_model.remove_one_cable(rand_battery_1, rand_house_1)
+        new_model.remove_one_cable(rand_battery_2, rand_house_2)
 
         # add new cables
-        new_model.add_one_cable(first_battery, mutate_house)
-        new_model.add_one_cable(mutate_battery, first_house)
+        new_model.add_one_cable(rand_battery_1, rand_house_2)
+        new_model.add_one_cable(rand_battery_2, rand_house_1)
 
-    def get_rand_house(self, new_model: Model, battery_idx: int):
-        """returns a random house"""
-        idx_rand_house = random.randint(0, (len(new_model.batteries[battery_idx].houses) - 1))
-        rand_house = new_model.batteries[battery_idx].houses[idx_rand_house]
+    def swap_houses(self, battery_1, battery_2, house_1, house_2):
+        battery_1.houses.append(house_2)
+        battery_2.houses.append(house_1)
+
+        house_1.battery = battery_2
+        house_2.battery = battery_1
+
+        idx_house_1 = battery_1.houses.index(house_1)
+        del battery_1.houses[idx_house_1]
+
+        idx_house_2 = battery_2.houses.index(house_2)
+        del battery_2.houses[idx_house_2]
+
+    def get_rand_house(self, new_model: Model, battery):
+        """Returns a random house from a randomly generated battery."""
+        idx = random.randint(0, (len(battery.houses) - 1))
+        rand_house = battery.houses[idx]
         return rand_house
 
-    def get_rand_battery_idx(self, new_model: Model) -> int:
-        """Returns random battery index"""
-        idx_rand_battery = random.randint(0, (len(new_model.batteries) - 1))
-        return idx_rand_battery
+    def get_rand_battery(self, new_model: Model):
+        """Returns a random battery from model's battery list.
+        Arguments:
+        new_model -- the model of input with existing connections
 
-    def get_rand_battery(self, new_model: Model, idx_rand_battery: int):
-        rand_battery = new_model.batteries[idx_rand_battery]
+        Returns:
+        rand_battery -- random generated battery object from battery list"""
+        idx = random.randint(0, (len(new_model.batteries) - 1))
+        rand_battery = new_model.batteries[idx]
         return rand_battery
 
     def mutate_model(self, new_model: Model) -> None:
-        """Changes all batteries randomly to random houses."""
+        """Changes all batteries randomly to random houses.
+        Arguments:
+        new_model --the model of input with existing connections"""
         # iterate over all batteries
         for i in range(5):
             self.mutate_battery_connection(new_model)
 
     def check_solution(self, new_model: Model) -> None:
-        """Checks if solution is a better solution than the best solution reached."""
+        """Checks if solution is a better solution than the best solution reached.
+        If solution has lower costs and is valid,
+        current best costs and model solution are switched to solution model.
+        Arguments:
+        new_model -- the model of input with existing connections."""
         # try new solution
         new_solution = new_model.calculate_costs()
-        old_solution = self.original_costs
+        old_solution = self.best_costs
 
         # if the costs are, change current model to new solution
         if new_model.is_solution() is False:
@@ -103,28 +123,25 @@ class Iteration():
 
         if new_solution < old_solution:
             print("found better solution")
-            self.original_costs = new_solution
+            self.best_costs = new_solution
             print(new_solution)
             self.model = new_model
             visualize(self.model)
 
     def run(self, iterations: int) -> None:
-        """Runs the hillclimber algorithm for a specific amount of iterations."""
+        """Runs the hillclimber algorithm for a specific amount of iterations.
+        First a copy is made of current model solution.
+        Then a mutation is made of current model solution.
+        If mutation model is better and valid, it becomes current best solution.
+        Arguments:
+        iterations -- the amount of times to run this function"""
 
         for iteration in range(iterations):
-            # print(f"Iteration {iteration}/{iterations}, current_value: {self.total_costs}") if verbose else None
-
             # make copy of model for mutations
             new_model = self.model.copy()
 
             # mutate model
             self.mutate_model(new_model)
 
-            # if new_model.calculate_costs() < current_best.calculate_costs():
-            #     print("better solution")
-            #     current_best = new_model
-            # else:
-            #     print(f"same{new_model.calculate_costs()} {new_model == current_best}")
-            # # set mutation model to cur
-            #rent model if more successful than current
+            # if solution is better and valid, change swap model to mutate model
             self.check_solution(new_model)
